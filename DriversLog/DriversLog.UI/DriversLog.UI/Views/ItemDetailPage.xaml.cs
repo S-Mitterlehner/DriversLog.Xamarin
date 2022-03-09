600d4b538e71b345a7098bc8f33b9b6ae581160d
Stefan Mitterlehner <stefanm1@gmx.at>
2022-03-09 21:01:33 +0100

    initial commit (template)

---
+++ DriversLog/DriversLog.UI/DriversLog.UI/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using DriversLog.UI.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace DriversLog.UI.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
